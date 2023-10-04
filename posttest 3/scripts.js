32823b89cf34e91600812bab2323059513cb21d3
Natan <natannaelprimuslung@gmail.com>
2023-10-04 23:19:48 +0800

    added more stuff

--- posttest 3/scripts.js
+++ posttest 3/scripts.js
@@ -1,9 +1,32 @@
+var welcome = prompt("Insert your name : ")
+alert("Welcome home Doctor " + welcome)
+
 function light_mode(){
 
-    document.body.classList.toggle("theme-light")
+    var theme_light = document.body;
+    theme_light.classList.toggle("theme-light")
 }
 
-function filter_invert(){
-    
-    document.getElementById("invert-icons").classList.toggle("filter-black")
+function lines(){
+    let e = document.createElement("div");
+    e.setAttribute("class", "circle");
+    document.body.appendChild(e)
 }
+
+
+function changeImage(){
+    
+    var img = document.getElementById("image-logo_arknights");
+    img.src = 'assets/images/logo_arknights-Black.svg'
+}
+
+var social_icon_hover = document.getElementById("social-icons");
+
+social_icon_hover.addEventListener("mouseover", function(){
+    social_icon_hover.style.transform = "translateY(-10px)";
+    console.log("working?")
+});
+
+social_icon_hover.addEventListener("mouseout", function(){
+    social_icon_hover.style.transform = "none";
+});
