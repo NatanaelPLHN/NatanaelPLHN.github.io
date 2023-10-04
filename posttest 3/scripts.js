56c601b231429cf667de969068ff7033b003d773
Natan <natannaelprimuslung@gmail.com>
2023-10-04 19:17:51 +0800

    Re-commit

---
+++ posttest 3/scripts.js
@@ -0,0 +1,9 @@
+function light_mode(){
+
+    document.body.classList.toggle("theme-light")
+}
+
+function filter_invert(){
+    
+    document.getElementById("invert-icons").classList.toggle("filter-black")
+}
